f3c14e3b7f5cdbc4518958a42540a4b340fd32a2
visharad Dhavle <visharad.dhavle@outlook.com>
2017-07-20 21:54:31 +0000

    Working with ansible

--- main.tf
+++ main.tf
@@ -41,14 +41,6 @@ resource "aws_route_table" "vishman_rt_public"{
 }
 
 
-# Private
-resource "aws_route_table" "vishman_rt_private"{
-  vpc_id = "${aws_vpc.vishman_vpc.id}"
-  tags{
-    Name = "vishman_rt_private"
-  }
-}
-
 # Subnet
 
 # public
@@ -72,37 +64,6 @@ resource "aws_subnet" "vishman_sn_public_b"{
   }
 }
 
-# private
-resource "aws_subnet" "vishman_sn_private_a"{
-  vpc_id = "${aws_vpc.vishman_vpc.id}"
-  cidr_block = "10.0.3.0/24"
-  map_public_ip_on_launch = false
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
-  tags{
-    Name = "vishman_sn_private_a"
-  }
-}
-
-resource "aws_subnet" "vishman_sn_private_b"{
-  vpc_id = "${aws_vpc.vishman_vpc.id}"
-  cidr_block = "10.0.4.0/24"
-  map_public_ip_on_launch = false
-  availability_zone = "${data.aws_availability_zones.available.names[1]}"
-  tags{
-    Name = "vishman_sn_private_b"
-  }
-}
-
-resource "aws_subnet" "vishman_sn_rds1"{
-  vpc_id = "${aws_vpc.vishman_vpc.id}"
-  cidr_block = "10.0.5.0/24"
-  map_public_ip_on_launch = false
-  availability_zone = "${data.aws_availability_zones.available.names[3]}"
-  tags{
-    Name = "vishman_sn_rds1"
-  }
-}
-
 
 # Route_Subnet Association
 
@@ -116,24 +77,6 @@ resource "aws_route_table_association" "rt_sn_public_b"{
   route_table_id = "${aws_route_table.vishman_rt_public.id}"
 }
 
-resource "aws_route_table_association" "rt_sn_private_a"{
-  subnet_id = "${aws_subnet.vishman_sn_private_a.id}"
-  route_table_id = "${aws_route_table.vishman_rt_private.id}"
-}
-
-resource "aws_route_table_association" "rt_sn_private_b"{
-  subnet_id = "${aws_subnet.vishman_sn_private_b.id}"
-  route_table_id = "${aws_route_table.vishman_rt_private.id}"
-}
-
-#resource "aws_db_subnet_group" "db_sn_rds"{
-# name = "db_sn_rds"
-#  subnet_ids = ["${aws_subnet.vishman_sn_rds1.id }"]  
-#  tags{
-#    Name = "db_sn_rds"
-#  }
-#}
-#For this group to work we need to add minimum two subnets to it
 
 #Securiry Groups
 resource "aws_security_group" "vishman_sg_public"{
@@ -163,35 +106,34 @@ resource "aws_security_group" "vishman_sg_public"{
 }
 
 
-resource "aws_security_group" "vishman_sg_private"{
-  name = "vishman_sg_private"
-  description = "This is private facing group"
-  vpc_id = "${aws_vpc.vishman_vpc.id}"
-  ingress{
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  egress{
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"] 
-  }
+# Key Pair
+resource "aws_key_pair" "vishman_auth_key"{
+  key_name = "${var.key_name}"
+  public_key = "${file(var.public_key_path)}"
 }
 
+# Server
 resource "aws_instance" "vishman_compute_dev"{
   instance_type = "${var.instance_type_dev_web}"
   ami = "${var.ami_web}"
   tags{
     Name = "vishman_compute_dev"
   }
+
+  key_name = "${aws_key_pair.vishman_auth_key.id}"
+  vpc_security_group_ids = ["${aws_security_group.vishman_sg_public.id}"]
+  subnet_id = "${aws_subnet.vishman_sn_public_a.id}"
+
+  provisioner "local-exec"{
+    command = <<EOD
+cat <<EOF > aws_hosts
+[dev]
+${aws_instance.vishman_compute_dev.public_ip}
+EOF
+EOD
+  }
+
+  provisioner "local-exec"{
+    command = "sleep 6m && ansible-playbook -i aws_hosts deploy.yml"
+  }
 }
-
-
-
-
-
-
--- variables.tf
+++ variables.tf
@@ -2,3 +2,5 @@ variable "aws_region" {}
 variable "aws_profile" {}
 variable "instance_type_dev_web" {}
 variable "ami_web" {}
+variable "key_name" {}
+variable "public_key_path" {}
